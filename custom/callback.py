927fffdf3a4bd58717031798c0a969253be20c5d
Shinnosuke Takasuka <73013416+shin-tksk@users.noreply.github.com>
2023-02-25 14:45:51 +0900

    Add files via upload

--- custom/callback.py
+++ custom/callback.py
@@ -1,49 +1,20 @@
-from tensorflow.python import keras
+#from tensorflow.python import keras
+import os
+os.environ["TF_FORCE_GPU_ALLOW_GROWTH"]= "true"
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 import params as par
 import sys
-from tensorflow.python.keras.optimizer_v2.learning_rate_schedule import LearningRateSchedule
-import os
-os.environ["TF_FORCE_GPU_ALLOW_GROWTH"]= "true"
-
-class MTFitCallback(keras.callbacks.Callback):
-
-    def __init__(self, save_path):
-        super(MTFitCallback, self).__init__()
-        self.save_path = save_path
-
-    def on_epoch_end(self, epoch, logs=None):
-        self.model.save(self.save_path)
-
-
-class TransformerLoss(keras.losses.SparseCategoricalCrossentropy):
-    def __init__(self, from_logits=False, reduction='none', debug=False,  **kwargs):
-        super(TransformerLoss, self).__init__(from_logits, reduction, **kwargs)
-        self.debug = debug
-        pass
-
-    def call(self, y_true, y_pred):
-        y_true = tf.cast(y_true, tf.int32)
-        mask = tf.math.logical_not(tf.math.equal(y_true, par.pad_token))
-        mask = tf.cast(mask, tf.float32)
-        _loss = super(TransformerLoss, self).call(y_true, y_pred)
-        _loss *= mask
-        if self.debug:
-            tf.print('loss shape:', _loss.shape, output_stream=sys.stdout)
-            tf.print('output:', tf.argmax(y_pred,-1), output_stream=sys.stdout)
-            tf.print(mask, output_stream=sys.stdout)
-            tf.print(_loss, output_stream=sys.stdout)
-        return _loss
+#from tensorflow.python.keras.optimizer_v2.learning_rate_schedule import LearningRateSchedule
+from tensorflow.keras.optimizers.schedules import LearningRateSchedule
 
 
 def transformer_dist_train_loss(y_true, y_pred):
     y_true = tf.cast(y_true, tf.int32)
     mask = tf.math.logical_not(tf.math.equal(y_true, par.pad_token))
     mask = tf.cast(mask, tf.float32)
-    #print(mask)
-
+    #print(y_pred[0])
     y_true_vector = tf.one_hot(y_true, par.vocab_size)
-
     _loss = tf.nn.softmax_cross_entropy_with_logits(y_true_vector, y_pred)
     #print(_loss.shape)
     #
@@ -52,11 +23,23 @@ def transformer_dist_train_loss(y_true, y_pred):
     #print(_loss)
     return _loss
 
-def classification_loss(y_true, y_pred):
-    #y_true y_pred batch*class
-    #y_true_vector = tf.one_hot(y_true, 2)
-    _loss = tf.nn.softmax_cross_entropy_with_logits(y_true, y_pred)
+def classification_loss(y_true, y_pred, real, pred):
+    lam1 = tf.cast(0, tf.float32)
+    lam2 = tf.cast(1.0, tf.float32)
 
+    real = tf.cast(real, tf.int32)
+    mask = tf.math.logical_not(tf.math.equal(real, par.pad_token))
+    mask = tf.cast(mask, tf.float32)
+    real = tf.one_hot(real, par.vocab_size)
+    mask_loss = tf.nn.softmax_cross_entropy_with_logits(real, pred)
+    mask_loss *= mask
+    mask_loss_ = tf.reduce_mean(mask_loss)
+
+    print('mask loss :', tf.reduce_mean(mask_loss_).numpy())
+    #print(y_true[:10])
+    _loss = tf.nn.softmax_cross_entropy_with_logits(y_true, y_pred)
+    #print('cla loss :', tf.reduce_mean(_loss).numpy())
+    _loss = tf.add(lam1 * tf.reduce_mean(mask_loss), lam2 * _loss)
     return _loss
 
 
--- custom/layers.py
+++ custom/layers.py
@@ -1,37 +1,18 @@
+import os
+os.environ["TF_FORCE_GPU_ALLOW_GROWTH"]= "true"
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 import math as m
-from tensorflow.python import keras
+import gc
+#from tensorflow.python import keras
 import numpy as np
 import math
-import os
+import sys
 import time
 import utils
-os.environ["TF_FORCE_GPU_ALLOW_GROWTH"]= "true"
 
-
-def sinusoid(max_seq, embedding_dim):
-    return np.array([[
-        [
-            m.sin(
-                pos * m.exp(-m.log(10000) * i / embedding_dim) * m.exp(
-                    m.log(10000) / embedding_dim * (i % 2)) + 0.5 * m.pi * (i % 2)
-            )
-            for i in range(embedding_dim)
-        ]
-        for pos in range(max_seq)
-    ]])
-
-class ExpandDims(keras.layers.Layer):
-    def __init__(self, axis=-1, **kwargs):
-        super().__init__(**kwargs)
-        self.axis = axis
-
-    def call(self, inputs, **kwargs):
-        return tf.expand_dims(inputs, axis=self.axis)
-
-# TODO : reduce time complexity
-class PositionEmbedding(keras.layers.Layer):
-    def __init__(self, max_seq, embedding_dim, **kwargs):
+class DynamicPositionEmbedding(tf.keras.layers.Layer):
+    def __init__(self, embedding_dim, max_seq=1024, **kwargs):
         super().__init__(**kwargs)
         embed_sinusoid_list = np.array([[
             [
@@ -46,56 +27,10 @@ class PositionEmbedding(keras.layers.Layer):
         self.positional_embedding = tf.constant(embed_sinusoid_list, dtype=tf.float32)
 
     def call(self, inputs, **kwargs):
-        return tf.add(inputs, self.positional_embedding)
-
-class PositionEmbeddingV2(keras.layers.Layer):
-    def __init__(self, max_seq, embedding_dim, **kwargs):
-        super(PositionEmbeddingV2, self).__init__(**kwargs)
-        angle_rads = PositionEmbeddingV2.__get_angles(np.arange(max_seq)[:, np.newaxis],
-                                                      np.arange(embedding_dim)[np.newaxis, :],
-                                                      embedding_dim)
-
-        # apply sin to even indices in the array; 2i
-        sines = np.sin(angle_rads[:, 0::2])
-
-        # apply cos to odd indices in the array; 2i+1
-        cosines = np.cos(angle_rads[:, 1::2])
-
-        pos_encoding = np.concatenate([sines, cosines], axis=-1)
-
-        pos_encoding = pos_encoding[np.newaxis, ...]
-
-        self.sinusoid = tf.cast(pos_encoding, dtype=tf.float32)
-
-    @staticmethod
-    def __get_angles(pos, i, d_model):
-        angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
-        return pos * angle_rates
-
-    def call(self, inputs, **kwargs):
-        return tf.add(inputs, self.sinusoid)
-
-class DynamicPositionEmbedding(keras.layers.Layer):
-    def __init__(self, embedding_dim, max_seq=2048, **kwargs):
-        super().__init__(**kwargs)
-        embed_sinusoid_list = np.array([[
-            [
-                m.sin(
-                    pos * m.exp(-m.log(10000) * i/embedding_dim) *
-                    m.exp(m.log(10000)/embedding_dim * (i % 2)) + 0.5 * m.pi * (i % 2)
-                )
-                for i in range(embedding_dim)
-            ]
-            for pos in range(max_seq)
-        ]])
-        self.positional_embedding = tf.constant(embed_sinusoid_list, dtype=tf.float32)
-
-    def call(self, inputs, **kwargs):
-        #print(inputs.shape,self.positional_embedding.shape)
         return tf.add(inputs, self.positional_embedding[:,:inputs.shape[1],:])
 
-class BaselineAttention(keras.layers.Layer):
-    def __init__(self, h, d, max_seq=2048, **kwargs):
+class BaselineAttention(tf.keras.layers.Layer):
+    def __init__(self, h, d, max_seq=1024, **kwargs):
         super().__init__(**kwargs)
         self.len_k = None
         self.max_seq = None
@@ -103,10 +38,10 @@ class BaselineAttention(keras.layers.Layer):
         self.h = h
         self.d = d
         self.dh = d // h
-        self.Wq = keras.layers.Dense(int(self.d / 2))
-        self.Wk = keras.layers.Dense(int(self.d / 2))
-        self.Wv = keras.layers.Dense(int(self.d))
-        self.fc = keras.layers.Dense(d)
+        self.Wq = tf.keras.layers.Dense(int(self.d / 2))
+        self.Wk = tf.keras.layers.Dense(int(self.d / 2))
+        self.Wv = tf.keras.layers.Dense(int(self.d))
+        self.fc = tf.keras.layers.Dense(d)
         self.max_seq = max_seq
 
     def build(self, input_shape):
@@ -154,12 +89,12 @@ class BaselineAttention(keras.layers.Layer):
 
         return out, attention_weights
 
-class RelativeGlobalAttention(keras.layers.Layer):
+class RelativeGlobalAttention(tf.keras.layers.Layer):
     """
     from Music Transformer ( Huang et al, 2018 )
     [paper link](https://arxiv.org/pdf/1809.04281.pdf)
     """
-    def __init__(self, h=4, d=256, add_emb=False, max_seq=2048, **kwargs):
+    def __init__(self, h=4, d=128, add_emb=False, max_seq=1024, **kwargs):
         super().__init__(**kwargs)
         self.len_k = None
         self.max_seq = max_seq
@@ -167,10 +102,10 @@ class RelativeGlobalAttention(keras.layers.Layer):
         self.h = h
         self.d = d
         self.dh = d // h
-        self.Wq = keras.layers.Dense(int(self.d))
-        self.Wk = keras.layers.Dense(int(self.d))
-        self.Wv = keras.layers.Dense(int(self.d))
-        self.fc = keras.layers.Dense(d)
+        self.Wq = tf.keras.layers.Dense(int(self.d))
+        self.Wk = tf.keras.layers.Dense(int(self.d))
+        self.Wv = tf.keras.layers.Dense(int(self.d))
+        self.fc = tf.keras.layers.Dense(d)
         self.additional = add_emb
         if self.additional:
             self.Radd = None
@@ -178,11 +113,9 @@ class RelativeGlobalAttention(keras.layers.Layer):
     def build(self, input_shape):
         self.shape_q = input_shape[0][1]
         self.shape_k = input_shape[1][1]
-        #print(input_shape[1][1])
-        # self.max_seq = max(input_shape[0][1], input_shape[1][1], input_shape[2][1])
         self.E = self.add_weight('emb', shape=[self.max_seq, int(self.dh)])
 
-    def call(self, inputs, mask=None, **kwargs):
+    def call(self, inputs, **kwargs):
         """
         :param inputs: a list of tensors. i.e) [Q, K, V]
         :param mask: mask tensor
@@ -216,27 +149,19 @@ class RelativeGlobalAttention(keras.layers.Layer):
         QKt = tf.matmul(q, Kt)
         logits = QKt + Srel
         logits = logits / math.sqrt(self.dh)
-        if mask is not None:
-            logits += (tf.cast(mask, tf.float32) * -1e9)
 
         attention_weights = tf.nn.softmax(logits, -1)
-        # tf.print('logit result: \n', logits, output_stream=sys.stdout)
-
         attention = tf.matmul(attention_weights, v)
-        # tf.print('attention result: \n', attention, output_stream=sys.stdout)
 
         out = tf.transpose(attention, (0, 2, 1, 3))
         out = tf.reshape(out, (out.shape[0], -1, self.d))
 
         out = self.fc(out)
-        #print(out.shape)
         return out, attention_weights
 
     def _get_left_embedding(self, len_q, len_k):
-        #print(self.E)
         starting_point = max(0,self.max_seq-len_q)
         e = self.E[starting_point:,:]
-        #print(e.shape)
         return e
 
     @staticmethod
@@ -244,184 +169,83 @@ class RelativeGlobalAttention(keras.layers.Layer):
         mask = tf.sequence_mask(
             tf.range(qe.shape[-1] -1, qe.shape[-1] - qe.shape[-2] -1, -1), qe.shape[-1])
         mask = tf.logical_not(mask)
-        #print(mask)
         mask = tf.cast(mask, tf.float32)
-
         return mask * qe
 
     def _skewing(self, tensor: tf.Tensor):
         padded = tf.pad(tensor, [[0, 0], [0,0], [0, 0], [1, 0]])
-        #print(tensor[0][0][2047])
         reshaped = tf.reshape(padded, shape=[-1, padded.shape[1], padded.shape[-1], padded.shape[-2]])
         Srel = reshaped[:, :, 1:, :]
-        # print('Sre: {}'.format(Srel))
         if self.len_k > self.len_q:
             Srel = tf.pad(Srel, [[0,0], [0,0], [0,0], [0, self.len_k-self.len_q]])
         elif self.len_k < self.len_q:
             Srel = Srel[:,:,:,:self.len_k]
-
         return Srel
 
-class View1D(keras.layers.Layer):
-    def __init__(self, axis=-1, **kwargs):
-        super().__init__(**kwargs)
-        self.axis = axis
-
-    def call(self, inputs, **kwargs):
-        return inputs[:,self.axis]
-
-class EncoderLayer(keras.layers.Layer):
-    def __init__(self, d_model, rate=0.1, h=16, additional=False, max_seq=1024):
+class EncoderLayer(tf.keras.layers.Layer):
+    def __init__(self, d_model, rate=0.1, h=4, additional=False, max_seq=1024):
         super(EncoderLayer, self).__init__()
 
         self.d_model = d_model
         self.rga = RelativeGlobalAttention(h=h, d=d_model, max_seq=max_seq, add_emb=additional)
 
-        self.FFN_pre = keras.layers.Dense(self.d_model // 2, activation=tf.nn.relu)
-        self.FFN_suf = keras.layers.Dense(self.d_model)
+        self.FFN_pre = tf.keras.layers.Dense(self.d_model // 2, activation=tf.nn.gelu)
+        self.FFN_suf = tf.keras.layers.Dense(self.d_model)
 
-        self.layernorm1 = keras.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
 
-        self.dropout1 = keras.layers.Dropout(rate)
-        self.dropout2 = keras.layers.Dropout(rate)
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
 
-    def call(self, x, mask=None, training=False, **kwargs):
-        #print(x.shape)
-        attn_out, w = self.rga([x,x,x], mask)
+    def call(self, x, training=False, **kwargs):
+
+        attn_out, w = self.rga([x,x,x])
         attn_out = self.dropout1(attn_out, training=training)
         out1 = self.layernorm1(attn_out+x)
-
         ffn_out = self.FFN_pre(out1)
         ffn_out = self.FFN_suf(ffn_out)
         ffn_out = self.dropout2(ffn_out, training=training)
         out2 = self.layernorm2(out1+ffn_out)
         return out2, w
 
-class DecoderLayer(keras.layers.Layer):
-    def __init__(self, d_model, rate=0.1, h=16, additional=False, max_seq=2048):
-        super(DecoderLayer, self).__init__()
 
-        self.d_model = d_model
-        self.rga2 = RelativeGlobalAttention(d=d_model, h=h, max_seq=max_seq, add_emb=additional)
-        self.rga = RelativeGlobalAttention(d=d_model, h=h, max_seq=max_seq, add_emb=additional)
-
-        self.FFN_pre = keras.layers.Dense(self.d_model // 2, activation=tf.nn.relu)
-        self.FFN_suf = keras.layers.Dense(self.d_model)
-
-        self.layernorm1 = keras.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = keras.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm3 = keras.layers.LayerNormalization(epsilon=1e-6)
-
-        self.dropout1 = keras.layers.Dropout(rate)
-        self.dropout2 = keras.layers.Dropout(rate)
-        self.dropout3 = keras.layers.Dropout(rate)
-
-    def call(self, x, encode_out, mask=None, lookup_mask=None, training=False, w_out=False, **kwargs):
-
-        attn_out, aw1 = self.rga([x, x, x], mask=lookup_mask)
-        attn_out = self.dropout1(attn_out, training=training)
-        out1 = self.layernorm1(attn_out+x)
-
-        if encode_out is None:
-            attn_out2, aw2 = self.rga2([out1, out1, out1], mask=mask)
-        else:
-            attn_out2, aw2 = self.rga2([out1, encode_out, encode_out], mask=mask)
-        attn_out2 = self.dropout2(attn_out2, training=training)
-        attn_out2 = self.layernorm2(out1+attn_out2)
-
-        ffn_out = self.FFN_pre(attn_out2)
-        ffn_out = self.FFN_suf(ffn_out)
-        ffn_out = self.dropout3(ffn_out, training=training)
-        out = self.layernorm3(attn_out2+ffn_out)
-
-        if w_out:
-            return out, aw1, aw2
-        else:
-            return out
-
-class Encoder(keras.layers.Layer):
+class Encoder(tf.keras.layers.Layer):
     def __init__(self, num_layers, d_model, input_vocab_size, rate=0.1, max_len=None):
         super(Encoder, self).__init__()
 
         self.d_model = d_model
         self.num_layers = num_layers
+        #self.num_layers = 8
         self.max_len = max_len
 
-        self.embedding = keras.layers.Embedding(input_vocab_size, d_model)
-        #self.bar_encoding = BarPositionEmbedding(self.d_model, max_seq=max_len, bar_id=None)
-        # if max_len is not None:
-        #     self.pos_encoding = PositionEmbedding(max_seq=max_len, embedding_dim=self.d_model)
-        if True:
-            self.pos_encoding = DynamicPositionEmbedding(self.d_model, max_seq=max_len)
+        self.embedding = tf.keras.layers.Embedding(input_vocab_size, d_model)
+        self.pos_embedding = tf.keras.layers.Embedding(max_len, d_model)
+        self.bar_embedding = tf.keras.layers.Embedding(50, d_model)
 
-        self.enc_layers = [EncoderLayer(d_model, rate, h=self.d_model // 64, additional=False, max_seq=max_len)
+        #if True:
+        #    self.pos_encoding = DynamicPositionEmbedding(self.d_model, max_seq=max_len)
+
+        self.enc_layers = [EncoderLayer(d_model, rate, h=self.d_model // 32, additional=False, max_seq=max_len)
                            for i in range(num_layers)]
-        self.dropout = keras.layers.Dropout(rate)
-        self.layernorm = keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout = tf.keras.layers.Dropout(rate)
+        self.layernorm = tf.keras.layers.LayerNormalization(epsilon=1e-6)
 
-    def call(self, x, bar_x, mask=None, training=False):
+    def call(self, x, bar_x, training=False):
         weights = []
-        # adding embedding and position encoding.
-        x = self.embedding(x)  # (batch_size, input_seq_len, d_model)
-        #print(tf.math.sqrt(tf.cast(self.d_model, tf.float32)))
+        x = self.embedding(x) 
         x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
-        x = self.pos_encoding(x)
-        x = self.bar_encoding(x,bar_x,self.d_model,self.max_len)
+        pos = np.arange(x.shape[0]*x.shape[1]).reshape(x.shape[0],x.shape[1]) % x.shape[1]
+        x = x + self.pos_embedding(pos) + self.bar_embedding(bar_x)
         x = self.dropout(x, training=training)
         x = self.layernorm(x)
 
         for i in range(self.num_layers):
-            x, w = self.enc_layers[i](x, mask, training=training)
+            #print(i,i,i,i,i,i,i)
+            x, w = self.enc_layers[i](x, training=training)
             weights.append(w)
-        #print(x.shape,w.shape,self.num_layers)
-        return x, weights  # (batch_size, input_seq_len, d_model)
-    
-    def bar_encoding(self,inputs,bar_id,embedding_dim,max_seq):
-        embed_sinusoid_list = np.array([
-            [
-                [
-                    m.sin(
-                        bar_id[b][pos] * m.exp(-m.log(10000) * i/embedding_dim) *
-                        m.exp(m.log(10000)/embedding_dim * (i % 2)) + 0.5 * m.pi * (i % 2)
-                    )
-                    for i in range(embedding_dim)
-                ]
-                for pos in range(max_seq)
-            ]
-            for b in range(inputs.shape[0])
-        ])
-        self.positional_embedding = tf.constant(embed_sinusoid_list, dtype=tf.float32)
-        return tf.add(inputs, self.positional_embedding[:,:inputs.shape[1],:])
 
-class Decoder(keras.layers.Layer):
-    def __init__(self, num_layers, d_model, input_vocab_size,
-                 rate=0.1, max_len=None):
-        super(Decoder, self).__init__()
-        self.d_model = d_model
-        self.num_layers = num_layers
-        self.embedding = keras.layers.Embedding(input_vocab_size, d_model)
-
-        if True:
-            self.pos_encoding = DynamicPositionEmbedding(self.d_model, max_seq=max_len)
-
-        self.dec_layers = [DecoderLayer(d_model, rate, h=self.d_model // 64, additional=False, max_seq=max_len)
-                           for i in range(num_layers)]
-        self.dropout = keras.layers.Dropout(rate)
-
-    def call(self, x, mask, lookup_mask, training, enc_output=None):
-        weights = []
-        # adding embedding and position encoding.
-        x = self.embedding(x)  # (batch_size, input_seq_len, d_model)
-        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
-        x = self.pos_encoding(x)
-        x = self.dropout(x, training=training)
-        for i in range(self.num_layers):
-            x, w1, w2 = \
-                self.dec_layers[i](x, enc_output, lookup_mask=lookup_mask, mask=mask, training=training, w_out=True)
-            weights.append((w1, w2))
-
-        return x, weights  # (batch_size, input_seq_len, d_model)
+        return x, weights
 
 if __name__ == '__main__':
     rga = RelativeGlobalAttention(d=9, h=1)
