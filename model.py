6d49da4522c7aaa8901a75c8e83ec0ee90bd39c5
Shinnosuke Takasuka <73013416+shin-tksk@users.noreply.github.com>
2023-03-05 18:16:50 +0900

    Update model.py

--- model.py
+++ model.py
@@ -191,17 +191,17 @@ class MusicTransformerDecoder(tf.keras.Model):
 
     def generate_mask(self, prior: list, length=1024):
         decode_array = prior
-        #for i in Bar('generating').iter(range(min(self.max_seq, length))):
-        for i in range(min(self.max_seq, length)):
+        for i in Bar('generating').iter(range(min(self.max_seq, length)-1)):
+        #for i in range(min(self.max_seq, length)-1):
             #print(decode_array[:20])
-            decode_array[i] = par.mask_token
+            decode_array[i+1] = par.mask_token
             decode_tensor = tf.constant([decode_array])
             result = self.call(decode_tensor, training=False)
-            print(result)
-            #pdf = tfp.distributions.Categorical(probs=result[:, i])
-            #result = pdf.sample(1).numpy()[0]
-            #decode_array[i] = result[0]
-            break
+            #print(result)
+            pdf = tfp.distributions.Categorical(probs=result[:, i+1])
+            result = pdf.sample(1).numpy()[0]
+            decode_array[i+1] = result[0]
+            #break
         return decode_array
 
     def generate_mask_pro(self, prior: list, length=2048):
